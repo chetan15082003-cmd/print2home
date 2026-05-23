5472ebeaeb2fc1e7b3ffc9861189fbff72ea3a00
Chetan Gupta <chetan15082003@gmail.com>
2026-05-23 06:59:59 +0530

    Add selectAndGo function for brand selection

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function selectAndGo(brand) {
+  sessionStorage.setItem('brand', brand);
+  window.location.href = 'issue.html';
+}
 const hamburger  = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobileMenu');
 hamburger.addEventListener('click', () => mobileMenu.classList.toggle('open'));
